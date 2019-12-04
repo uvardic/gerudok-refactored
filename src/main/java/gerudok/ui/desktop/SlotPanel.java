7fc0fc881ec7fcbbdf6cffe222164e11982a4af5
uros <uros.vardiic@gmail.com>
2019-12-04 22:57:12 +0100

    Scroll zoom

--- src/main/java/gerudok/ui/desktop/SlotPanel.java
+++ src/main/java/gerudok/ui/desktop/SlotPanel.java
@@ -15,6 +15,12 @@ import java.awt.geom.Point2D;
 
 public class SlotPanel extends JPanel {
 
+    private static final double SCALING_FACTOR = 1.2;
+
+    private static final double MIN_SCALING_FACTOR = 0.2;
+
+    private static final double MAX_SCALING_FACTOR = 5;
+
     private final Slot model;
 
     private final AffineTransform transformationMatrix = new AffineTransform();
@@ -41,6 +47,20 @@ public class SlotPanel extends JPanel {
 
     public AffineTransform getTransformationMatrix() {
         return transformationMatrix;
+    }
+
+    public void zoomIn() {
+        double scalingFactor = boundScale(transformationMatrix.getScaleX()) * SCALING_FACTOR;
+
+        Point2D oldPosition = new Point(getWidth() / 2, getHeight() / 2);
+
+    }
+
+    private double boundScale(double scalingFactor) {
+        if (scalingFactor < MIN_SCALING_FACTOR)
+            return MIN_SCALING_FACTOR;
+
+        return Math.min(scalingFactor, MAX_SCALING_FACTOR);
     }
 
     private class Canvas extends JPanel {
@@ -93,11 +113,11 @@ public class SlotPanel extends JPanel {
 
         @Override
         public void mousePressed(MouseEvent event) {
-            transformMousePosition(event.getPoint());
+            transformPosition(event.getPoint());
             stateManager.getCurrentState().mousePressed(event);
         }
 
-        private void transformMousePosition(Point2D position) {
+        private void transformPosition(Point2D position) {
             try {
                 transformationMatrix.inverseTransform(position, position);
             } catch (NoninvertibleTransformException e) {
@@ -109,10 +129,46 @@ public class SlotPanel extends JPanel {
         public void mouseWheelMoved(MouseWheelEvent event) {
             DiagramFrame selectedDiagramFrame = Desktop.getInstance().getSelectedDiagramFrame();
 
-            if (isShiftPressed(event))
+            if (isControlPressed(event))
+                mouseWheelZoom(event);
+            else if (isShiftPressed(event))
                 selectedDiagramFrame.adjustScrollBars(0, event.getWheelRotation());
             else
                 selectedDiagramFrame.adjustScrollBars(event.getWheelRotation(), 0);
+        }
+
+        private boolean isControlPressed(MouseWheelEvent event) {
+            return (event.getModifiersEx() & InputEvent.CTRL_DOWN_MASK) != 0;
+        }
+
+        private void mouseWheelZoom(MouseWheelEvent event) {
+            double scale = calculateNewScale(event);
+
+            Point2D originalPosition = event.getPoint();
+            transformPosition(originalPosition);
+
+            transformationMatrix.setToScale(scale, scale);
+
+            Point2D newPosition = event.getPoint();
+            transformPosition(newPosition);
+
+            transformationMatrix.translate(
+                    newPosition.getX() - originalPosition.getX(),
+                    newPosition.getY() - originalPosition.getY()
+            );
+        }
+
+        private double calculateNewScale(MouseWheelEvent event) {
+            double scale = boundScale(transformationMatrix.getScaleX());
+
+            if (isScrollWheelZoomingOut(event))
+                return scale / (event.getWheelRotation() * SCALING_FACTOR);
+            else
+                return scale * (-event.getWheelRotation() * SCALING_FACTOR);
+        }
+
+        private boolean isScrollWheelZoomingOut(MouseWheelEvent event) {
+            return event.getWheelRotation() > 0;
         }
 
         private boolean isShiftPressed(MouseWheelEvent event) {
